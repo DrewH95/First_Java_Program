6cd09c59694c5b547bc14f98aff8f6a1e6d5e75b
DrewH95 <andrew.j.hughes333@gmail.com>
2021-11-30 14:07:42 -0500

    Commit

---
+++ First.java
@@ -0,0 +1,8 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.println("My name is Drew Hughes");
+        System.out.println("I am 26 years old");
+        System.out.println("My hometown is Pittsburgh, PA");
+    }
+
+}
